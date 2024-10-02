2da29340697a11658fb908a6d4f43b1bdf99202e
ChouChiaYing <168095441+ChouChiaYing@users.noreply.github.com>
2024-10-02 11:38:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,14 +10,26 @@ from linebot.models import *
 
 #======python的函數庫==========
 import tempfile, os
-
+import openai
+import time
 import traceback
-
+import json
+import random
 import requests
-#======python的函數庫==========
 
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.language.questionanswering import QuestionAnsweringClient
+
+from datetime import datetime,timezone,timedelta
+
+app = Flask(__name__)
+
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+
+# Set up LINE_bot
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+
 
 # OPENAI API Key初始化設定
 endpoint = os.getenv('END_POINT')
@@ -30,20 +42,12 @@ headers = {
     "api-key": open_ai_api_key,
 }
 
-app = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-# Channel Access Token
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-# Channel Secret
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
-
-
-endpoint = os.getenv('END_POINT')
+# Set up Language Studio
 credential = AzureKeyCredential(os.getenv('AZURE_KEY'))
 knowledge_base_project = os.getenv('PROJECT')
 deployment = 'production'
 
-
+# 連接Azure Language Studio，查詢知識庫
 def QA_response(text):
     client = QuestionAnsweringClient(endpoint, credential)
     with client:
@@ -56,21 +60,7 @@ def QA_response(text):
     return output.answers[0].answer
 
 
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
+# 連接Azure OpenAI的Chatgpt
 def Chatgpt_response(prompt):   
 
     # Define the payload for the request
@@ -101,40 +91,48 @@ def Chatgpt_response(prompt):
         print(f"Error {response.status_code}: {response.text}")
 
 
-# 處理訊息
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+# 處理文字訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-
-    try:
-        QA_answer = QA_response(msg)
-        print(QA_answer)
-        if QA_answer!='No good match found in KB':
+    if msg[0]=='!':
+        try:
+            gpt_answer = Chatgpt_response(msg)
+            print(gpt_answer)
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(gpt_answer))
+        except:
+            print(traceback.format_exc())
+            line_bot_api.reply_message(event.reply_token, TextSendMessage('Please retry later'))       
+    else:
+        try:
+            QA_answer = QA_response(msg)
+            print(QA_answer)        
             line_bot_api.reply_message(event.reply_token, TextSendMessage(QA_answer))
-        else:
-             QA_answer = Chatgpt_response(msg)
-             line_bot_api.reply_message(event.reply_token, TextSendMessage(QA_answer))
-    except:
-        print(traceback.format_exc())
-        line_bot_api.reply_message(event.reply_token, TextSendMessage('QA Error'))
-        
+        except:
+            print(traceback.format_exc())
+            line_bot_api.reply_message(event.reply_token, TextSendMessage('QA Error'))
+
 
 @handler.add(PostbackEvent)
 def handle_message(event):
     print(event.postback.data)
-
-
-@handler.add(MemberJoinedEvent)
-def welcome(event):
-    uid = event.joined.members[0].user_id
-    gid = event.source.group_id
-    profile = line_bot_api.get_group_member_profile(gid, uid)
-    name = profile.display_name
-    message = TextSendMessage(text=f'{name}歡迎加入')
-    line_bot_api.reply_message(event.reply_token, message)
-        
-        
-import os
+      
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
